9f4f51ff852f16f520e715c56ac4ff61e464df55
Maddy Pye <madeline.pye@selu.edu>
2023-09-26 19:13:37 +0000

    commit prior changes to revert

--- pye.pkg/R/remove_na.R
+++ pye.pkg/R/remove_na.R
@@ -2,6 +2,6 @@ remove_nas <- function(data, columns){
   clean <- data %>% 
     na.omit(data) %>% 
     select(all_of({{columns}}))
-  return(clea)}
+  return(clean)}
 
 # make sure to set a variable equal to the column names that you want to select for
